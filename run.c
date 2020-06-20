4d151241ffeb51d40dbe695034a5872af669d0cb
Luis Ferrufino <33588812+lferru@users.noreply.github.com>
2020-06-20 19:02:04 -0400

    Create run.c
    Just getting started.

---
+++ run.c
@@ -0,0 +1,13 @@
+/** Author: Luis Ferrufino
+  * Date: 6/20/20
+  * File: run.c
+  * Project: declension-engine
+  ** /
+  
+  #include <stdio.h>
+  
+  int main(void) {
+  
+    printf("hi")
+    
+  }
